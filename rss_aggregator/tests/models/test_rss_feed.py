5934a0ca9483cac9b58f5193b406d82f906bff3f
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-05-02 18:53:40 -0400

    all tests for rss_feed OK, except for update -- Diamond CRUDMixin must be updated

--- rss_aggregator/tests/models/test_rss_feed.py
+++ rss_aggregator/tests/models/test_rss_feed.py
@@ -16,7 +16,7 @@ class RSSFeedTestCase(DiamondTestCase):
     def tearDown(self):
         super().tearDown()
 
-    @attr('single')
+    # @attr('single')
     def test_create(self):
         "Testing rss_aggregator.models.RSSFeed.create"
         models.RSSFeed.create(
@@ -25,3 +25,45 @@ class RSSFeedTestCase(DiamondTestCase):
         )
         retrieved = models.RSSFeed.find(name="Generic Feed")
         self.assertIsNotNone(retrieved, "Object retrievible.")
+
+    # @attr('single')
+    def test_read(self):
+        "Testing rss_aggregator.models.RSSFeed.find"
+        models.RSSFeed.create(
+            name="Generic Feed",
+            rss_url="https://www.genericfeed.com/feed.xml",
+        )
+
+        retrieved = models.RSSFeed.find(name="Generic Feed")
+        self.assertEquals(retrieved.rss_url, "https://www.genericfeed.com/feed.xml", "Attributes readable.")
+
+    # @attr('single')
+    def test_update(self):
+        "Testing rss_aggregator.models.RSSFeed.update"
+        models.RSSFeed.create(
+            name="Generic Feed",
+            rss_url="https://www.genericfeed.com/feed.xml",
+        )
+
+        retrieved = models.RSSFeed.find(name="Generic Feed")
+        new_attribute = "null"
+        retrieved.update(rss_url=new_attribute)
+
+        retrieved = models.RSSFeed.find(name="Generic Feed")
+        self.assertEquals(retrieved.rss_url, new_attribute, "Attributes updated.")
+
+    # @attr('single')
+    def test_delete(self):
+        "Testing rss_aggregator.models.RSSFeed.delete"
+        models.RSSFeed.create(
+            name="Generic Feed",
+            rss_url="https://www.genericfeed.com/feed.xml",
+        )
+
+        retrieved = models.RSSFeed.find(name="Generic Feed")
+        self.assertIsNotNone(retrieved, "Object retrievible.")
+
+        retrieved.delete()
+
+        retrieved = models.RSSFeed.find(name="Generic Feed")
+        self.assertIsNone(retrieved, "Object deleted.")
