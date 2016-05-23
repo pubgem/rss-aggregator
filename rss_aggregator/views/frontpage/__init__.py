3abde442a45b8e6f36e37480bdcf388ac160dcef
Ian Dennis Miller <ian@iandennismiller.com>
2016-05-23 17:48:48 -0400

    substantial progress on front page

--- rss_aggregator/views/frontpage/__init__.py
+++ rss_aggregator/views/frontpage/__init__.py
@@ -5,11 +5,11 @@ frontpage = flask.Blueprint(
     'frontpage',
     __name__,
     template_folder='templates',
-    static_folder='static'
+    static_folder='static-frontpage'
 )
 
 
-@frontpage.route('/frontpage')
+@frontpage.route('/')
 def index():
     return flask.render_template('frontpage/index.html', rss_entries=models.RSSEntry.query.all())
     # return flask.render_template('index.html', admin_view=None, rss_entry=None)
