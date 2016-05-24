50fc143c76e87e4b8d1c57f91d051d338c65e0c1
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-05-23 20:50:34 -0400

    diff

--- rss_aggregator/models/pubgem_user.py
+++ rss_aggregator/models/pubgem_user.py
@@ -22,6 +22,16 @@ class PubgemUser(User):
     def subscribe(self, *rss_feeds):
         """
         Subscribes a PubgemUser to an RSSFeed.
+
+        :param *rss_feeds: a list of models.RSSFeed objects
+        """
+        valid_feeds = [i for i in rss_feeds if i not in self.subscriptions]  # Removes any duplicate subscriptions
+        self.subscriptions.extend(valid_feeds)
+        # import ipdb; ipdb.set_trace()
+
+    def unsubscribe(self, *rss_feeds):
+        """
+        Unsubscribes a PubgemUser from an RSSFeed.
         """
         valid_feeds = [i for i in rss_feeds if i not in self.subscriptions]  # Removes any duplicate subscriptions
         self.subscriptions.extend(valid_feeds)
